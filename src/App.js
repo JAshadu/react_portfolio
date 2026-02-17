97485ed093427977cbfdbbf24333c11bfefbfd96
JB_Ash <jashadu123@gmail.com>
2026-02-17 11:50:59 +0000

    Position and add text for header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header></header>
+      <header>
+        <a>{/*Consider having a logo, <img src='https://placehold.co/50'></img>*/}JAshadu</a>
+        <nav>
+          <a>About</a>
+          <a>Projects</a>
+          <a>CV</a>
+          <a>Contact</a>
+        </nav>
+      </header>
       <main></main>
       <footer></footer>
     </div>
