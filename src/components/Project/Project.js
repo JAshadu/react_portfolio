d3da42b020ec70094bd71ae3ca934f9c6d9744cd
JB_Ash <jashadu123@gmail.com>
2026-02-20 21:13:46 +0000

    Fix openCard and closeCard function

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,25 +1,29 @@
 import './Project.css'
-import { useRef } from 'react';
+import { useState } from 'react';
 import ProjectCard from '../ProjectCard/ProjectCard';
 
 
 function Project(props) {
-    const containerRef = useRef()
+    const [portfolioState, setPortfolioState] = useState(false)
 
     const openCard = () => {
-        containerRef.current.style.display = 'flex'
+        setPortfolioState(true)
+    }
+
+    const closeCard = () => {
+        setPortfolioState(false)
     }
 
     return(
         <div className="project-preview">
-            <a onClick={openCard}><img src={`${props.img}`} alt={`Preview of ${props.title} project`}></img></a>
-            <ProjectCard
+            <button onClick={openCard}><img src={`${props.img}`} alt={`Preview of ${props.title} project`}></img></button>
+            {portfolioState && (<ProjectCard
             title={props.title}
             img={props.img}
             links={props.links}
             description={props.description}
             skills={props.skills}
-            containerRef={containerRef} />
+            closeCard={closeCard} />)}
         </div>
     )
 }
--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -4,21 +4,23 @@ import './ProjectCard.css';
 function ProjectCard(props) {
     const closeRef = useRef()
 
-    const closeCard = () => {
-        props.containerRef.current.style.display = 'none'
+    const closePortfolioCard = (e) => {
+        if(props.links.project === '/#') {
+            props.closeCard()
+        }
     }
 
     return(
-        <div className='project-card-container' ref={props.containerRef}>
+        <div className='project-card-container'>
             <div className='project-card'>
                 <h3>{props.title}</h3>
-                <span ref={closeRef} onClick={closeCard}>X</span>
+                <span ref={closeRef} onClick={props.closeCard}>X</span>
                 <div className='project-card-middle'>
                     <div className='project-links'>
-                        {props.links.project && (<a className='project-link' href={props.links.project}>Open Project</a>)}
+                        {props.links.project && (<a className='project-link' href={props.links.project} onClick={closePortfolioCard}>Open Project</a>)}
                         {props.links.github && (<a className='project-github' href={props.links.github} target='_blank' rel='noopener noreferrer'>GitHub Repository</a>)}
                     </div>
-                    <img src={props.img}></img>
+                    <img src={props.img} alt={`Preview of ${props.title} project`}></img>
                     <div className='project-skills'>Import Tech Stack Component</div>
                 </div>
                 <p>{props.description}</p>
